5c6a6c1149f1587f9d05ffbcd8323a0933776f99
olaf.spiewok <olaf.spiewok@exozet.com>
2020-03-25 21:34:58 +0100

    * add some new test component

---
+++ Packages/com.ospiewok.upm-ci-template/Runtime/SomeCoolScripts.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace UpmTestExample.Local
+{
+    public class SomeNewComponent : MonoBehaviour
+    {
+        void Start() 
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
